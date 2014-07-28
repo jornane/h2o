1c53d6bc8fa4f7cee132ca7a142524d88c456709
yorn <git@yorn.priv.no>
2014-07-28 10:39:16 +0200

    Added example preprocessors

---
+++ src/yorn/h2o/processing/htmlescapepreprocessor.php
@@ -0,0 +1,20 @@
+<?php namespace yorn\h2o\processing;
+
+use \yorn\h2o\content\Document;
+
+/**
+ * Preprocessor that will escape HTML markup.
+ *
+ * @author Jørn Åne <i@jornane.no>
+ * @copyright Copyright 2014 Jørn Åne
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+class HTMLEscapePreprocessor implements Preprocessor {
+
+	public function preprocess( Document $document ) {
+		$document->setPreprocessedContent( htmlspecialchars( $document->getRawContent() ) );
+		$document->setPreprocessedTitle( htmlspecialchars( $document->getRawTitle() ) );
+	}
+
+}
---
+++ src/yorn/h2o/processing/verbatimpreprocessor.php
@@ -0,0 +1,20 @@
+<?php namespace yorn\h2o\processing;
+
+use \yorn\h2o\content\Document;
+
+/**
+ * Preprocessor that will use the raw input word for word.
+ *
+ * @author Jørn Åne <i@jornane.no>
+ * @copyright Copyright 2014 Jørn Åne
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+class VerbatimPreprocessor implements Preprocessor {
+
+	public function preprocess( Document $document ) {
+		$document->setPreprocessedContent( $document->getRawContent() );
+		$document->setPreprocessedTitle( $document->getRawTitle() );
+	}
+
+}
